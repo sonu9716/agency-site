e54a6a388f58c74721210bb7d9dea572889b729f
sonusingh <sonusingh2972001@gmail.com>
2026-03-02 02:14:57 +0530

    Fix express routing paths for static Next.js export

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const PORT = process.env.PORT || 3000;
 // Serve the main agency site at the root
 app.use(express.static(path.join(__dirname, 'agency-site')));
 
-// Serve demo-site (dental clinic) at /demo1
+// Serve demo-site (dental clinic) at /demo1. Since we configured basePath: '/demo1', 
+// Next.js handles the routing correctly as long as we serve the folder. 
+// We just need to map `/demo1` to the out folder and let it serve the index.html
 app.use('/demo1', express.static(path.join(__dirname, 'demo-site', 'out')));
+app.get('/demo1/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'demo-site', 'out', 'index.html'));
+});
 
 // Serve demo-site2 (women's clinic) at /demo2
 app.use('/demo2', express.static(path.join(__dirname, 'demo-site2', 'out')));
+app.get('/demo2/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'demo-site2', 'out', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
